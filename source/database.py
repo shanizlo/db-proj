cc747bb8dbd76f5b4cc884c201a68c58a3100465
shanizlo <shanizlotnik@gmail.com>
2021-09-16 15:40:36 +0300

    Add fixes to db, added types to functions parameters, added handling exception for insert song, fixed returning ids, added some test prints

--- source/database.py
+++ source/database.py
@@ -3,8 +3,6 @@ import sqlite3
 from source import TxtParser
 from source.entities import *
 
-# _conn = sqlite3.Connection
-# _cursor = sqlite3.Cursor
 connection = sqlite3.connect(":memory:")
 cursor = connection.cursor()
 
@@ -15,37 +13,51 @@ cursor.executescript(sql_as_string)
 
 def insert_into_database(author, title, album, copyright, lyrics):
     the_song = Song(author, title, album, copyright)  # making the song for inserting
-    song_id = insert_song(the_song)  # inserting the song and getting its id for the contains
-    the_words_with_context = TxtParser.parse(lyrics)  # the parsed lyrics with all their information
+    # If there was an error inserting the song - don't continue
+    try:
+        song_id = insert_song(the_song)  # inserting the song and getting its id for the contains
+        the_words_with_context = TxtParser.parse(lyrics)  # the parsed lyrics with all their information
 
-    for wc in the_words_with_context:
-        insert_word(Word(wc[0]))
-        word_id = get_id_from_word(wc[0])
-        insert_contains(word_id, song_id, wc[1], wc[2], wc[3])
+        for wc in the_words_with_context:
+            insert_word(Word(wc[0]))
+            word_id = get_id_from_word(wc[0])
+            insert_contains(word_id, song_id, wc[1], wc[2], wc[3])
+
+    except sqlite3.Error as err:
+        return err
 
 
-def insert_song(song):
+def insert_song(song: Song):
     with connection:
-        cursor.execute("INSERT INTO songs VALUES (:song_id, :author, :title, :album, :copyright)",
-                       {'song_id': None, 'author': song.author, 'title': song.title, 'album': song.album,
-                        'copyright': song.copyright})
-        cursor.execute("SELECT  last_insert_rowid();")
-        return cursor.fetchall()[0][0]
+        try:
+            cursor.execute("INSERT INTO songs VALUES (:song_id, :author, :title, :album, :copyright) returning song_id;",
+                           {'song_id': None, 'author': song.author, 'title': song.title, 'album': song.album,
+                            'copyright': song.copyright})
+            # returning id of the last insert
+            return cursor.fetchone()[0]
+        except sqlite3.Error as err:
+            print("Error occured: ", err)
+            return 0
 
 
-def insert_word(word):
+def insert_word(word: Word):
     with connection:
-        cursor.execute("INSERT OR IGNORE INTO words VALUES (:word_id, :word_value, :word_length)",
+        cursor.execute("INSERT OR IGNORE INTO words VALUES (:word_id, :word_value, :word_length) returning word_id;",
                        {'word_id': None, 'word_value': word.value, 'word_length': word.length})
+        inserted_word_id = cursor.fetchone()
+        # This is needed in case word ignored and there no word_id created
+        if inserted_word_id is not None:
+            return inserted_word_id[0]
+        else:
+            return None
 
 
-def get_id_from_word(str):
+def get_id_from_word(word_str: str):
     with connection:
-        cursor.execute("SELECT word_id FROM words WHERE word_value = \"" + str + "\"")
-        id = cursor.fetchone()
-        # id = cursor.fetchall()
-        # return id[0][0]
-        return id[0]
+        # cursor.execute("SELECT word_id FROM words WHERE word_value = %s",
+        cursor.execute("SELECT word_id FROM words WHERE word_value = (:word_str)",
+                       {'word_str': word_str})
+        return cursor.fetchone()[0]
 
 
 def insert_contains(word_id, song_id, verse_num, sentence_num, word_position):
@@ -57,8 +69,9 @@ def insert_contains(word_id, song_id, verse_num, sentence_num, word_position):
 
 def create_group(group_name):
     with connection:
-        cursor.execute("INSERT INTO groups VALUES (:group_id, :group_name)",
+        cursor.execute("INSERT INTO groups VALUES (:group_id, :group_name) returning group_id;",
                        {'group_id': None, 'group_name': group_name.group_name})
+        return cursor.fetchone()[0]
 
 # TODO: add words to group - find group by id and add words
 #
@@ -69,12 +82,39 @@ def create_group(group_name):
 
 
 ###### Tests ######## Tests ######### Tests ######### Tests ############ Tests ###############
+copyright_test = "copyright There are plenty of tutorials on the 877834 2948923"
+album_test = "Some Album 13"
+author_test = "Shani Zlotnik"
+title_test = "Title Chuck"
+lyrics_test = """There are plenty of tutorials on the internet on using unittest but something I couldn’t find while doing a project was a tutorial on how to mock a database for testing.
+I believe a similar method can be used for pytest as well.
+When doing continual 
+testing as the software is developed or improved upon, testing has to be done to ensure expected functionality.
+There may be methods or functions that can alter the data in the database. 
+When testing these functions, it’s best to use a separate database. 
+It’s most definitely not recommended to use the production database while testing.
+When testing is automated it’s not possible to manually change the database 
+that each function is using. So, it’s necessary to patch in the test database to the production database. 
+For that, we use the patch function available in the mock package. This is available in the Python standard library, available as unittest.mock, but for this tutorial, we’ll be using the mock package."""
+
 word1 = Word("hello")
 group1 = Group("chuck")
-insert_word(word1)
-insert_contains(1, 1, 1, 1, 1)
-create_group(group1)
-insert_into_database("chuck", "title", "alb", "cpergjj5", "hello import sqlite3  \n hjgsdf")
+insert_into_database(author_test, title_test, album_test, copyright_test, lyrics_test)
+insert_into_database(author_test, title_test, album_test, copyright_test, lyrics_test)
+song1 = Song(author_test, title_test, album_test, copyright_test)
+
+# song1Id = insert_song(song1)
+# print("song_id: ", song1Id)
+print("printing song")
+for row in cursor.execute("SELECT * FROM songs"):
+    print(row)
+
+# word1_id = insert_word(word1)
+# print("inserted word, the id: ", word1_id)
+
+# id1 = get_id_from_word("hello")
+# insert_contains(1, 1, 1, 1, 1)
+# create_group(group1)
 
 print(word1.value)
 
