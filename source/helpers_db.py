8b68b824339a26c102e205d55b9692c80111fec6
shanizlo <shanizlotnik@gmail.com>
2021-11-15 21:46:56 +0200

    context wip

--- source/helpers_db.py
+++ source/helpers_db.py
@@ -48,3 +48,33 @@ def SearchWordByPositionInSong(author, title, verseNum, lineNum, wordNum):
         return "Word with this position not found."
     return search_word_by_id(word_id)
 
+def ReturnWordContext(author: str, title: str, word_value):
+    # get all wordId where value=value and song_id=song_id
+    # TODO: find a way to reuse song_id from a previos search and maybe words as well
+    song_id = search_song_id(author, title)
+    # get all words from the song
+    words_ids = search_words_ids_song_contains(song_id)
+    relevant_word_ids = []
+    # get all word_ids with relevant values (they all will have same id but different positions)
+    for w_id in words_ids:
+        w_value = search_word_by_id(w_id[0])
+        if (w_value is not None) and (w_value == word_value):
+            relevant_word_ids.append(w_id)
+    # relevant_word_ids.sort()
+    context_list = []
+    # find context for each
+    for w_id in relevant_word_ids:
+        this_w_verse = w_id[2]
+        this_w_line = w_id[3]
+        this_w_pos = w_id[4]
+        # returns id_s of words in this line
+        this_w_context = []
+        word_ids_in_this_line = get_words_in_line(song_id, this_w_verse, this_w_line)
+        for id in word_ids_in_this_line:
+            val = search_word_by_id(id[0])
+            this_w_context.append(val)
+        this_w_context_as_text = "\n".join(this_w_context)
+        this_w_context.append(' (verse: %s, line: %s)\n' %(this_w_verse, this_w_line))
+        context_list.append(this_w_context)
+    context_as_text = "\n".join(context_list)
+    return context_as_text
